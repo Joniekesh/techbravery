9e913fada0d6bca887649e0c25a6aab1e0b9a8ae
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-01-02 08:15:26 +0100

    Commit

--- frontend/src/components/welcome/Welcome.jsx
+++ frontend/src/components/welcome/Welcome.jsx
@@ -1,4 +1,12 @@
 import "./welcome.scss";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import { items } from "../../utils/data";
+// ..
+AOS.init({
+  // throttleDelay: 300,
+  duration: 2000,
+});
 
 const Welcome = () => {
   return (
@@ -19,111 +27,21 @@ const Welcome = () => {
       </div>
       <h2>Why Choose Us?</h2>
       <div className="items">
-        <div className="item">
-          <div className="left">
-            <h3>Unleash Your Digital Vision:</h3>
-            <p>
-              We believe in turning your ideas into reality. Our team of
-              creative wizards works closely with you to understand your unique
-              vision and translate it into a visually stunning and fully
-              functional website that reflects your brand identity.
-            </p>
+        {items.map((item, i) => (
+          <div
+            className="item"
+            data-aos={i % 2 === 0 ? "zoom-in" : "fade-right"}
+            key={item.id}
+          >
+            <div className="left">
+              <h3>{item.title}:</h3>
+              <p>{item.desc}</p>
+            </div>
+            <div className="right">
+              <img src={item.img} alt="" />
+            </div>
           </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Seamless User Experience:</h3>
-            <p>
-              We are obsessed with user experience (UX). Our websites are built
-              with meticulous attention to detail, ensuring intuitive
-              navigation, fast load times, and responsive design across all
-              devices. We create an immersive journey for your visitors,
-              maximizing engagement and conversions.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Customized Solutions:</h3>
-            <p>
-              No two businesses are alike, and we understand that. Our web
-              development solutions are custom-tailored to meet your specific
-              needs and objectives. Whether you require an e-commerce platform,
-              a content management system, or a sleek portfolio website, we have
-              the expertise to bring your vision to life.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Cutting-Edge Technologies:</h3>
-            <p>
-              Our team stays ahead of the curve by keeping up with the latest
-              industry trends and technologies. We leverage the power of HTML5,
-              CSS3, JavaScript frameworks, and CMS platforms to deliver
-              cutting-edge websites that are both visually stunning and
-              technologically advanced.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Collaborative Partnership:</h3>
-            <p>
-              We believe in building lasting relationships with our clients. We
-              value open communication, transparency, and collaboration
-              throughout the entire development process. Your satisfaction is
-              our top priority, and we work tirelessly to ensure your project is
-              delivered on time and within budget.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Results-Driven Approach:</h3>
-            <p>
-              We don't just build websites; we build strategies to drive
-              results. Our team of digital strategists will work closely with
-              you to understand your business goals and develop a comprehensive
-              plan to maximize your online presence. From search engine
-              optimization (SEO) to conversion rate optimization (CRO), we
-              employ data-driven techniques to achieve measurable success.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
-        <div className="item">
-          <div className="left">
-            <h3>Ongoing Support:</h3>
-            <p>
-              Ongoing Support: Our commitment to your success doesn't end at
-              launch. We provide ongoing support and maintenance services to
-              ensure your website remains secure, up-to-date, and optimized for
-              peak performance. We are always just a call or email away whenever
-              you need us.
-            </p>
-          </div>
-          <div className="right">
-            <img src="/assets/screen4.jpg" alt="" />
-          </div>
-        </div>
+        ))}
       </div>
       <div className="summary">
         <p>
