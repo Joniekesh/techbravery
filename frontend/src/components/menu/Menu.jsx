3f1deb71b1d86a9626be75b18c31adb974196d86
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-01-09 18:33:15 +0100

    Commit

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -3,13 +3,15 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 import { FaArrowRight } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
-import { menuData } from "../../utils/menuData";
+import { services, teams, works } from "../../utils/menuData";
 import { RxCaretDown } from "react-icons/rx";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Menu = ({ setOpenMenu }) => {
-  const [toggle, setToggle] = useState(false);
+  const [serviceToggle, setServiceToggle] = useState(false);
+  const [worksToggle, setWorksToggle] = useState(false);
+  const [teamToggle, setTeamToggle] = useState(false);
 
   return (
     <div className="menu">
@@ -21,32 +23,72 @@ const Menu = ({ setOpenMenu }) => {
           <span>TechBravery</span>
         </div>
         <div className="menuItems">
-          {menuData.map((item) => (
-            <div className="itemsContainer" key={item.id}>
-              <div className="titleContainer">
-                <h2 className="title">{item.name}</h2>
-                <span className="icon" onClick={() => setToggle(!toggle)}>
-                  <RxCaretDown />
-                </span>
-              </div>
-              {!toggle && (
+          <div className="item">
+            <div className="itemTitle">
+              <h2>Our Services</h2>
+              <span
+                className="icon"
+                onClick={() => setServiceToggle((prev) => !prev)}
+              >
+                <RxCaretDown />
+              </span>
+            </div>
+            {serviceToggle && (
+              <>
+                {services.map((service) => (
+                  <Link key={service.id} to={service.url} className="title">
+                    {service.name}
+                  </Link>
+                ))}
+              </>
+            )}
+          </div>
+          <div className="item">
+            <div className="itemTitle">
+              <h2>Our Works</h2>
+              <span
+                className="icon"
+                onClick={() => setWorksToggle((prev) => !prev)}
+              >
+                <RxCaretDown />
+              </span>
+            </div>
+            {worksToggle && (
+              <>
+                {works.map((work) => (
+                  <Link key={work.id} className="title">
+                    {work.name}
+                  </Link>
+                ))}
+              </>
+            )}
+          </div>
+          <div className="item">
+            <div className="itemTitle">
+              <h2>Team Members</h2>
+              <span
+                className="icon"
+                onClick={() => setTeamToggle((prev) => !prev)}
+              >
+                <RxCaretDown />
+              </span>
+            </div>
+            <div className="team">
+              {teamToggle && (
                 <>
-                  {item.data.map((inner) => (
-                    <Link
-                      onClick={() => setOpenMenu(false)}
-                      to={inner.url}
-                      className="innerTitle"
-                      key={inner.id}
-                    >
-                      {inner.name}
-                      {inner.position}
-                      <img src={inner.pic} alt="" />
-                    </Link>
+                  {teams.map((team) => (
+                    <div className="teamItem" key={team.id}>
+                      <img src={team.pic} alt="" />
+                      <div className="teamContainer">
+                        <span className="name">{team.name}</span>
+                        <span className="position">{team.position}</span>
+                      </div>
+                    </div>
                   ))}
                 </>
               )}
             </div>
-          ))}
+          </div>
         </div>
         <button className="quoteBtn">
           <div className="text">Get a Quote</div>
--- frontend/src/utils/menuData.js
+++ frontend/src/utils/menuData.js
@@ -1,145 +1,138 @@
-export const menuData = [
+export const services = [
   {
     id: 1,
-    name: "Our Services",
-    data: [
-      {
-        id: 1,
-        name: "Responsive Website Design",
-        url: "/",
-      },
-      {
-        id: 2,
-        name: "UI/UX Design",
-        url: "/",
-      },
-      {
-        id: 3,
-        name: "Graphic Design",
-        url: "/",
-      },
-      {
-        id: 4,
-        name: "E-Commerce Development",
-        url: "/",
-      },
-      {
-        id: 5,
-        name: "Website Development",
-        url: "/",
-      },
-      {
-        id: 6,
-        name: "Front-end Development",
-        url: "/",
-      },
-      {
-        id: 7,
-        name: "Back-end Development",
-        url: "/",
-      },
-      {
-        id: 8,
-        name: "Search Engine Optimizatio (SEO)",
-        url: "/",
-      },
-      {
-        id: 9,
-        name: "CMS Services",
-        url: "/",
-      },
-      {
-        id: 10,
-        name: "Website Security",
-        url: "/",
-      },
-      {
-        id: 11,
-        name: "Website Redesign",
-        url: "/",
-      },
-      {
-        id: 12,
-        name: "Mobile App Design and Development",
-        url: "/",
-      },
-      {
-        id: 13,
-        name: "Social Media Integration",
-        url: "/",
-      },
-      {
-        id: 14,
-        name: "Web Accessibility",
-        url: "/",
-      },
-      {
-        id: 15,
-        name: "Content Creation",
-        url: "/",
-      },
-    ],
+    name: "Responsive Website Design",
+    url: "/",
   },
   {
     id: 2,
-    name: "Our Works",
-    data: [
-      {
-        id: 1,
-        name: "Jonie Chat",
-        url: "/",
-      },
-      {
-        id: 2,
-        name: "Jonie POS",
-        url: "/",
-      },
-      {
-        id: 3,
-        name: "Jonie Shopy",
-        url: "/",
-      },
-      {
-        id: 4,
-        name: "Jonie AI",
-        url: "/",
-      },
-    ],
+    name: "UI/UX Design",
+    url: "/",
   },
   {
     id: 3,
-    name: "Our Team Members",
-    data: [
-      {
-        id: 1,
-        name: "Jonie Dev",
-        position: "Backend Engineer",
-        pic: "/assets/man.jpg",
-      },
-      {
-        id: 2,
-        name: "Lucy Dev",
-        position: "Frontend Engineer",
-        pic: "/assets/woman1.jpg",
-      },
-      {
-        id: 3,
-        name: "Sam Smith",
-        position: "Product Analyst",
-        pic: "/assets/man2.jpg",
-      },
-      {
-        id: 4,
-        name: "Thomas Man",
-        position: "UI/UX Designer",
-        pic: "/assets/man3.jpg",
-      },
-      {
-        id: 5,
-        name: "Kate Shaw",
-        position: "Customer Care Rep",
-        pic: "/assets/woman2.jpg",
-      },
-    ],
+    name: "Graphic Design",
+    url: "/",
+  },
+  {
+    id: 4,
+    name: "E-Commerce Development",
+    url: "/",
+  },
+  {
+    id: 5,
+    name: "Website Development",
+    url: "/",
+  },
+  {
+    id: 6,
+    name: "Front-end Development",
+    url: "/",
+  },
+  {
+    id: 7,
+    name: "Back-end Development",
+    url: "/",
+  },
+  {
+    id: 8,
+    name: "Search Engine Optimizatio (SEO)",
+    url: "/",
+  },
+  {
+    id: 9,
+    name: "CMS Services",
+    url: "/",
+  },
+  {
+    id: 10,
+    name: "Website Security",
+    url: "/",
+  },
+  {
+    id: 11,
+    name: "Website Redesign",
+    url: "/",
+  },
+  {
+    id: 12,
+    name: "Mobile App Design and Development",
+    url: "/",
+  },
+  {
+    id: 13,
+    name: "Social Media Integration",
+    url: "/",
+  },
+  {
+    id: 14,
+    name: "Web Accessibility",
+    url: "/",
+  },
+  {
+    id: 15,
+    name: "Content Creation",
+    url: "/",
+  },
+];
+
+export const works = [
+  {
+    id: 1,
+    name: "Jonie Chat",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Jonie POS",
+    url: "/",
+  },
+  {
+    id: 3,
+    name: "Jonie Shopy",
+    url: "/",
+  },
+  {
+    id: 4,
+    name: "Jonie AI",
+    url: "/",
+  },
+];
+
+export const teams = [
+  {
+    id: 1,
+    name: "Jonie Dev",
+    position: "Backend Engineer",
+    pic: "/assets/man.jpg",
+    url: "/",
+  },
+  {
+    id: 2,
+    name: "Lucy Dev",
+    position: "Frontend Engineer",
+    pic: "/assets/woman1.jpg",
+    url: "/",
+  },
+  {
+    id: 3,
+    name: "Sam Smith",
+    position: "Product Analyst",
+    pic: "/assets/man2.jpg",
+    url: "/",
+  },
+  {
+    id: 4,
+    name: "Thomas Man",
+    position: "UI/UX Designer",
+    pic: "/assets/man3.jpg",
+    url: "/",
+  },
+  {
+    id: 5,
+    name: "Kate Shaw",
+    position: "Customer Care Rep",
+    pic: "/assets/woman2.jpg",
+    url: "/",
   },
 ];
