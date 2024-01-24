f250400291fd98eadf3d52c8c47146ed115c5912
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-01-24 17:16:27 +0100

    Commit

--- frontend/src/pages/works/Works.jsx
+++ frontend/src/pages/works/Works.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import "./works.scss";
 
 const Works = () => {
-  return <div>Works</div>;
+  return (
+    <div className="works">
+      <div className="container">
+        <h1>Our Works</h1>
+        <p>
+          Welcome to our works catalogues. Take your time and check previous
+          delivered projects and choose any model of your choice and we will
+          costomize to suit your needs. <br />
+          <br />
+          Welcome to <span>Our Works</span> page where we showcase diverse range
+          of projects we have successfully completed for clients across various
+          industries. Each project represents a unique challenge that we
+          addressed with our expertise and creativity. Through detailed case
+          studies, we highlight the specific requirements, solutions
+          implemented, and the positive impact our software has had on our
+          clients' businesses. From mobile applications that revolutionized
+          customer experiences to complex enterprise systems that streamlined
+          operations. Our portfolio demonstrates our ability to deliver results
+          that drive business growth and success. <br />
+          <br /> We take pride in our track records of delivering high-quality
+          solutions that exceed client expectations. Our portfolio not only
+          showcases our technical capabilities but also serves as a testament to
+          the strong partnerships we have built with our clients. We believe
+          that the success of a project lies in understanding our clients' goals
+          and collaborating closely with them throughout the entire development
+          process. Get inspired by our success stories and see how we can help
+          you achieve your software development objectives.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Works;
