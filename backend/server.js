c0377a64bdea95e76c6e006be5a438426773a60e
Okoro John Darlington <okorojohndarlington@gmail.com>
2024-02-02 16:47:24 +0100

    Commit

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin: "https://techbravery.netlify.app",
+    // origin: "http://localhost:5173",
+    origin: "https://techbravery.netlify.app",
     credentials: true,
   })
 );
