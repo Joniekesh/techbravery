7ce58b68d15df7f09edacb89ac7c08e8a657b905
Joniekesh <okorojohndarlington@gmail.com>
2025-06-18 16:25:44 +0100

    commit

--- frontend/src/components/chatButton/ChatButton.jsx
+++ frontend/src/components/chatButton/ChatButton.jsx
@@ -3,7 +3,11 @@ import "./chatButton.scss";
 const ChatButton = () => {
   return (
     <div className="chatButton">
-      <a href="http://whatsapp.com" target="_blank">
+      <a
+        href="https://wa.me/+2348050667156?text=urlencodedtext"
+        target="_blank"
+        rel="noreferrer"
+      >
         <img src="/chatbtnicon3.jpg" alt="" />
       </a>
     </div>
